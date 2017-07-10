5e97adcb2350b9c98be5fa5f06483af507f38e59
Patrik Spathon <patrik@spathon.com>
2017-07-10 20:14:07 +0200

    Sort by newest in top

--- routes/index.js
+++ routes/index.js
@@ -5,13 +5,18 @@ const { Apartment } = require('../models')
 
 /* GET home page. */
 router.get('/', async (req, res, next) => {
-  const result = await Apartment.findAll()
+  let result
+  try {
+    result = await Apartment.findAll({ order: [ ['createdAt', 'DESC'] ] })
+  } catch (e) {
+    console.log(e)
+    throw e
+  }
   const apartments = result.map(instance => {
     const apartment = Object.assign({}, instance.toJSON())
     apartment.createdAt = format(apartment.createdAt, 'D MMMM YYYY - HH:mm')
     return apartment
   })
-  console.log(apartments)
   res.render('index', { title: 'Apartments in Chamonix', apartments })
 })
 
